9edd93560da9022314469bf758a323eef32163d7
David Eastmond <davebuck1981@gmail.com>
2022-04-12 10:02:16 -0400

    import some icons used for journal and jEntries

---
+++ src/components/SearchResultsPopup/components/ResultPanel.tsx
@@ -0,0 +1,6 @@
+import JournalIcon from "@mui/icons-material/Book";
+import JournalEntryIcon from "@mui/icons-material/Article";
+
+export function ResultPanel() {
+  return <div className="ResultPanel__Main"></div>;
+}
