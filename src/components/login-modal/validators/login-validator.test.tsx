fba42965c531d8b605b2b6fd4da9f2b3657cbc13
David Eastmond <davebuck1981@gmail.com>
2022-07-01 14:59:21 -0400

    login validator func tests

---
+++ src/components/login-modal/validators/login-validator.test.tsx
@@ -0,0 +1,69 @@
+import { validateLoginData } from "./login-validator";
+describe("login validator tests", () => {
+  test("fail conditions: no e-mail provided", () => {
+    const failEmailCondition = jest.fn();
+    validateLoginData({
+      email: null as any,
+      password: "password123",
+      onFail: failEmailCondition,
+      onSuccess: () => console.log("something else"),
+    });
+    expect(failEmailCondition).toHaveBeenCalled();
+    expect(failEmailCondition).toBeCalledWith({
+      field: "email",
+      message: "Enter an e-mail",
+    });
+  });
+  test("fail conditions: invalid e-mail", () => {
+    const failEmailCondition = jest.fn();
+    validateLoginData({
+      email: "e.mail",
+      password: "password123",
+      onFail: failEmailCondition,
+      onSuccess: () => console.log("something else"),
+    });
+    expect(failEmailCondition).toHaveBeenCalled();
+    expect(failEmailCondition).toHaveBeenCalledWith({
+      field: "email",
+      message: "Please enter a valid e-mail address",
+    });
+  });
+  test("fail condition: password is null", () => {
+    const failPasswordCondition = jest.fn();
+    validateLoginData({
+      email: "test@example.com",
+      password: null as any,
+      onFail: failPasswordCondition,
+      onSuccess: () => console.log("something else"),
+    });
+    expect(failPasswordCondition).toHaveBeenCalled();
+    expect(failPasswordCondition).toHaveBeenCalledWith({
+      field: "password",
+      message: "Enter a password that is at least 8 characters long",
+    });
+  });
+  test("fail condition: password is empty string", () => {
+    const failPasswordCondition = jest.fn();
+    validateLoginData({
+      email: "           ",
+      password: null as any,
+      onFail: failPasswordCondition,
+      onSuccess: () => console.log("something else"),
+    });
+    expect(failPasswordCondition).toHaveBeenCalled();
+    expect(failPasswordCondition).toHaveBeenCalledWith({
+      field: "password",
+      message: "Enter a password that is at least 8 characters long",
+    });
+  });
+  test("success condition is called", () => {
+    const successCondition = jest.fn();
+    validateLoginData({
+      email: "test@example.com",
+      password: "password$12345",
+      onFail: () => console.log("something failed"),
+      onSuccess: successCondition,
+    });
+    expect(successCondition).toHaveBeenCalled();
+  });
+});
