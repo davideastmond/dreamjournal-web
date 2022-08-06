4dadead501dd656e4b68a7b466fa21e5e8eb7795
David Eastmond <davebuck1981@gmail.com>
2022-08-06 14:00:33 -0400

    imports, styling

--- src/scenes/ProfileSettings/GridRow.tsx
+++ src/scenes/ProfileSettings/GridRow.tsx
@@ -1,4 +1,6 @@
 import { Grid, TextField } from "@mui/material";
+import { TextFieldProps } from "@mui/material/TextField";
+import { styled } from "@mui/material/styles";
 import { Item } from "./Item";
 
 export const GridRow = ({
@@ -7,18 +9,30 @@ export const GridRow = ({
   contentData,
   onEdit,
   idTag,
+  component,
 }: {
   canEdit: boolean;
   label: string;
   contentData?: string;
   onEdit?: (value: string) => void;
   idTag?: string;
+  component?: JSX.Element;
 }) => {
   const handleOnTextInputChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
     onEdit && onEdit(event.target.value);
   };
+
+  const StyledTextField = styled(TextField)<TextFieldProps>(() => ({
+    "&&& input": {
+      height: "0",
+      width: "100%",
+    },
+    "&.MuiTextField-root": {
+      width: "100%",
+    },
+  }));
 
   return (
     <>
@@ -27,14 +41,16 @@ export const GridRow = ({
       </Grid>
       {canEdit ? (
         <Grid item xs={6}>
-          <Item>
-            <TextField
+          {component ? (
+            component
+          ) : (
+            <StyledTextField
               variant="outlined"
               value={contentData}
               onChange={handleOnTextInputChange}
               id={idTag}
             />
-          </Item>
+          )}
         </Grid>
       ) : (
         <Grid item xs={6}>
