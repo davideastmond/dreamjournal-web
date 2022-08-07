56536de9b06ad6e011425ab6f6575ddb2ebdd21d
David Eastmond <davebuck1981@gmail.com>
2022-08-06 20:34:02 -0400

    journal list styling

--- src/components/JournalList/JournalList.tsx
+++ src/components/JournalList/JournalList.tsx
@@ -1,34 +1,77 @@
-import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import {
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  styled,
+  Typography,
+} from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import { TJournal } from "../../services/journal/journal.types";
+import AddIcon from "@mui/icons-material/Add";
 import "./style.css";
+import { pallet } from "../../pallets";
 
 interface IJournalListProps {
   journals: TJournal[];
 }
-function JournaList(props: IJournalListProps) {
+
+const StyledAddNewJournalButton = styled(Button)((props) => ({
+  backgroundColor: pallet.redWine,
+  [props.theme.breakpoints.down("sm")]: {
+    display: "none",
+  },
+}));
+
+const StyledAddIcon = styled(AddIcon)((props) => ({
+  color: pallet.redWine,
+  [props.theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+  "&:hover": {
+    cursor: "pointer",
+  },
+}));
+
+const StyledHeader = styled("header")((props) => ({
+  [props.theme.breakpoints.down("sm")]: {
+    display: "flex",
+    justifyContent: "space-evenly",
+  },
+}));
+
+const StyledList = styled(List)(() => ({
+  marginLeft: "20%",
+  marginRight: "20%",
+}));
+
+function JournalList(props: IJournalListProps) {
   const handleListItemClick = ({ journalId }: { journalId: string }) => {
-    //window.location.href = `/journals/$${journalId}`
     navigate(`/journals/${journalId}`, { replace: true });
   };
   const navigate = useNavigate();
   return (
     <div className="JournaList__Main">
-      <Typography
-        id="journalsTitle"
-        variant="h5"
-        sx={{
-          color: "white",
-        }}
-      >
-        Journals
-      </Typography>
-      <div className="NewJournals__Controls">
-        <div className="warning-color cursor-hover">
-          <Link to="/new_journal">+ New Journal</Link>
+      <StyledHeader className="JournalList__Main__div top-margin-buffer">
+        <Typography
+          id="journalsTitle"
+          variant="h5"
+          sx={{
+            color: "white",
+          }}
+        >
+          Journals
+        </Typography>
+        <div className="NewJournals__Controls">
+          <StyledAddNewJournalButton variant="outlined">
+            <Link to="/new_journal">New Journal</Link>
+          </StyledAddNewJournalButton>
+          <StyledAddIcon color="primary">
+            <Link to="/new_journal">New Journal</Link>
+          </StyledAddIcon>
         </div>
-      </div>
-      <List dense={true}>
+      </StyledHeader>
+      <StyledList dense={true}>
         {props.journals &&
           props.journals.length > 0 &&
           props.journals.map((journalItem) => (
@@ -46,9 +89,9 @@ function JournaList(props: IJournalListProps) {
             <ListItemText primary="No journals">No Journals</ListItemText>
           </ListItem>
         )}
-      </List>
+      </StyledList>
     </div>
   );
 }
 
-export default JournaList;
+export default JournalList;
