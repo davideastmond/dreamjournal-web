9cc4a27c3cf86a05bab90c90ef1f06c3526047c5
David Eastmond <davebuck1981@gmail.com>
2022-08-09 18:56:47 -0400

    journal list refactor

--- src/components/JournalList/JournalList.tsx
+++ src/components/JournalList/JournalList.tsx
@@ -1,16 +1,17 @@
 import {
   Button,
+  IconButton,
   List,
   ListItem,
   ListItemText,
   styled,
-  Typography,
 } from "@mui/material";
+import JournalBookMarkIcon from "@mui/icons-material/CollectionsBookmark";
 import { Link, useNavigate } from "react-router-dom";
 import { TJournal } from "../../services/journal/journal.types";
 import AddIcon from "@mui/icons-material/Add";
-import "./style.css";
-import { pallet } from "../../pallets";
+import { pallet } from "../../styling/pallets";
+import { StyledHeaderComponent } from "../StyledHeader";
 
 interface IJournalListProps {
   journals: TJournal[];
@@ -23,21 +24,19 @@ const StyledAddNewJournalButton = styled(Button)((props) => ({
   },
 }));
 
-const StyledAddIcon = styled(AddIcon)((props) => ({
+const StyledAddIconButton = styled(IconButton)((props) => ({
   color: pallet.redWine,
   [props.theme.breakpoints.up("sm")]: {
     display: "none",
   },
   "&:hover": {
     cursor: "pointer",
+    color: pallet.eggShellWhite,
+    backgroundColor: pallet.redWine,
   },
-}));
-
-const StyledHeader = styled("header")((props) => ({
-  [props.theme.breakpoints.down("sm")]: {
-    display: "flex",
-    justifyContent: "space-evenly",
-  },
+  paddingTop: "5px",
+  border: "1px solid",
+  borderRadius: "20%",
 }));
 
 const StyledList = styled(List)(() => ({
@@ -45,48 +44,73 @@ const StyledList = styled(List)(() => ({
   marginRight: "20%",
 }));
 
+const StyledListItemText = styled(ListItemText)((props) => ({
+  color: pallet.eggShellWhite,
+  marginLeft: "10px",
+  [props.theme.breakpoints.down("sm")]: {
+    "&& .MuiTypography-root": {
+      fontSize: "1rem",
+    },
+  },
+  "& .MuiTypography-root": {
+    fontSize: "1.5rem",
+  },
+}));
+
+const StyledJournalBookMarkIcon = styled(JournalBookMarkIcon)(() => ({
+  color: pallet.lightSalmon,
+}));
+
+const StyledListItem = styled(ListItem)(() => ({
+  "&:hover": {
+    cursor: "pointer",
+  },
+}));
+
 function JournalList(props: IJournalListProps) {
   const handleListItemClick = ({ journalId }: { journalId: string }) => {
     navigate(`/journals/${journalId}`, { replace: true });
   };
   const navigate = useNavigate();
+
+  const generateAddJournalButtons = () => {
+    return (
+      <div className="NewJournals__Controls">
+        <StyledAddNewJournalButton variant="outlined">
+          <Link to="/new_journal">New Journal</Link>
+        </StyledAddNewJournalButton>
+        <StyledAddIconButton color="primary">
+          <Link to="/new_journal">
+            <AddIcon />
+          </Link>
+        </StyledAddIconButton>
+      </div>
+    );
+  };
   return (
     <div className="JournaList__Main">
-      <StyledHeader className="JournalList__Main__div top-margin-buffer">
-        <Typography
-          id="journalsTitle"
-          variant="h5"
-          sx={{
-            color: "white",
-          }}
-        >
-          Journals
-        </Typography>
-        <div className="NewJournals__Controls">
-          <StyledAddNewJournalButton variant="outlined">
-            <Link to="/new_journal">New Journal</Link>
-          </StyledAddNewJournalButton>
-          <StyledAddIcon color="primary">
-            <Link to="/new_journal">New Journal</Link>
-          </StyledAddIcon>
-        </div>
-      </StyledHeader>
+      <StyledHeaderComponent text="Your journals">
+        {generateAddJournalButtons()}
+      </StyledHeaderComponent>
       <StyledList dense={true}>
         {props.journals &&
           props.journals.length > 0 &&
           props.journals.map((journalItem) => (
-            <ListItem key={journalItem._id}>
-              <ListItemText
+            <StyledListItem key={journalItem._id}>
+              <StyledJournalBookMarkIcon />
+              <StyledListItemText
                 primary={journalItem.title}
                 onClick={() =>
                   handleListItemClick({ journalId: journalItem._id })
                 }
               />
-            </ListItem>
+            </StyledListItem>
           ))}
         {(!props.journals || props.journals.length === 0) && (
           <ListItem>
-            <ListItemText primary="No journals">No Journals</ListItemText>
+            <ListItemText primary="No journals">
+              You don't have any journals
+            </ListItemText>
           </ListItem>
         )}
       </StyledList>
