56f74a0c3faaa8cb068e1055ff68e8e417245e24
David Eastmond <davebuck1981@gmail.com>
2022-08-27 13:46:49 -0400

    remove security question functionality

--- src/scenes/ProfileSettings/ProfileSettings.tsx
+++ src/scenes/ProfileSettings/ProfileSettings.tsx
@@ -1,25 +1,15 @@
-import { Tab, Tabs, styled, Fade } from "@mui/material";
+import { Tab, Tabs, styled } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { TabPanel } from "../../components/tab-panel";
 
 import { useSelector, shallowEqual } from "react-redux";
 import { selectSessionUser } from "../../reducers/app-slice";
 import "./style.css";
-import { createNewUserSecurityQuestions } from "../../services/user/user.service";
-import {
-  SecurityQuestionPrompter,
-  SecurityQuestionSelector,
-} from "../../components/SecurityQuestion";
-import { getSecurityQuestionsForUserByUserId } from "../../services/authentication/authentication.security.service";
-import {
-  TNewSecurityQuestionDataSubmission,
-  TSecurityQuestionTemplate,
-} from "../../services/authentication/authentication.types";
+
 import { Spinner } from "../../components/Spinner";
 import { PersonalPanel } from "./PersonalPanel";
 import TwoFactorAuthModule from "./TwoFactorAuthModule";
 import { pallet } from "../../styling/pallets";
-import { StyledHeaderComponent } from "../../components/StyledHeader";
 
 import { PasswordSecurityPanel } from "./components/password-security-panel";
 function a11yProps(index: number) {
@@ -60,12 +50,6 @@ const StyledTabPanel = styled(TabPanel)((props) => ({
 function ProfileSettings() {
   const [value, setValue] = useState<number>(0);
   const sessionUser = useSelector(selectSessionUser, shallowEqual);
-  const [hasSecurityQuestionsSet, setHasSecurityQuestionsSet] =
-    useState<boolean>(false);
-  const [securityQuestionData, setSecurityQuestionData] = useState<{
-    isSet: boolean;
-    questions: TSecurityQuestionTemplate[];
-  } | null>(null);
   const [isUpdateMode, setIsUpdateMode] = useState<boolean>(false);
   const [isDoingNetworkRequest, setIsDoingNetworkRequest] =
     useState<boolean>(false);
@@ -74,45 +58,6 @@ function ProfileSettings() {
     setValue(newValue);
   };
 
-  const getRefreshSecurityQuestions = async () => {
-    if (sessionUser) {
-      setIsDoingNetworkRequest(true);
-      const data = await getSecurityQuestionsForUserByUserId({
-        userId: sessionUser?._id,
-      });
-      setHasSecurityQuestionsSet(data.isSet);
-      setSecurityQuestionData(data);
-      setIsDoingNetworkRequest(false);
-      setIsUpdateMode(false);
-    }
-  };
-
-  useEffect(() => {
-    getRefreshSecurityQuestions();
-  }, []);
-
-  const handleSubmitNewSecurityQuestions = async (
-    data: TNewSecurityQuestionDataSubmission
-  ) => {
-    if (!sessionUser) return;
-    try {
-      await createNewUserSecurityQuestions({
-        userId: sessionUser._id,
-        data,
-      });
-      getRefreshSecurityQuestions();
-    } catch (exception: any) {
-      console.log(exception.message);
-      setIsDoingNetworkRequest(false);
-    }
-  };
-  const handlePrompterEditButtonClick = () => {
-    setIsUpdateMode(true);
-  };
-
-  const handleSecurityQuestionSelectorCancelClicked = () => {
-    setIsUpdateMode(false);
-  };
   return (
     <div style={{ backgroundColor: "black" }}>
       {isDoingNetworkRequest && <Spinner />}
@@ -143,37 +88,6 @@ function ProfileSettings() {
             isUpdateMode ? "security-panel-update-hidden" : ""
           }`}
         />
-        <div className="security-question-section top-divider-border">
-          <StyledHeaderComponent text="Security questions" sizeVariant="h5" />
-          <section>
-            {hasSecurityQuestionsSet &&
-              securityQuestionData &&
-              !isUpdateMode && (
-                <SecurityQuestionPrompter
-                  data={securityQuestionData}
-                  editButtonVisible={true}
-                  onEditButtonClick={handlePrompterEditButtonClick}
-                />
-              )}
-            {!hasSecurityQuestionsSet && !isUpdateMode && (
-              <SecurityQuestionSelector
-                onSaveSubmit={handleSubmitNewSecurityQuestions}
-                onCancelClicked={handleSecurityQuestionSelectorCancelClicked}
-              />
-            )}
-            {hasSecurityQuestionsSet && isUpdateMode && (
-              <SecurityQuestionSelector
-                onSaveSubmit={handleSubmitNewSecurityQuestions}
-                onCancelClicked={handleSecurityQuestionSelectorCancelClicked}
-                existingQuestions={
-                  securityQuestionData
-                    ? securityQuestionData.questions.map((q) => q.prompt)
-                    : []
-                }
-              />
-            )}
-          </section>
-        </div>
       </StyledTabPanel>
       <TabPanel value={value} index={2}>
         {sessionUser && <TwoFactorAuthModule sessionUserId={sessionUser._id} />}
--- src/services/authentication/authentication.security.service.ts
+++ src/services/authentication/authentication.security.service.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import { API_URL, AUTH_HEADER } from "../../environment";
 import {
   TGetTwoFactorEnabledResponse,
-  TNewSecurityQuestionDataSubmission,
   TSecurityQuestionTemplate,
 } from "./authentication.types";
 
@@ -21,52 +20,6 @@ export const getAvailableSecurityQuestions = async (): Promise<
       },
     });
     return req.data as TSecurityQuestionTemplate[];
-  } catch (exception: any) {
-    throw new Error(exception.response.data.error);
-  }
-};
-
-export const getSecurityQuestionsForUserByUserId = async ({
-  userId,
-}: {
-  userId: string;
-}): Promise<{ isSet: boolean; questions: TSecurityQuestionTemplate[] }> => {
-  const token = sessionStorage.getItem("token");
-  try {
-    const req = await axios({
-      method: "GET",
-      url: `${API_URL}/api/user/${userId}/security`,
-      withCredentials: true,
-      headers: {
-        ...AUTH_HEADER,
-        "X-JWT-Token": token!,
-      },
-    });
-    return req.data;
-  } catch (exception: any) {
-    throw new Error(exception.response.data.error);
-  }
-};
-
-export const sendSecurityQuestionsSelection = async ({
-  userId,
-  data,
-}: {
-  data: TNewSecurityQuestionDataSubmission;
-  userId: string;
-}): Promise<void> => {
-  const token = sessionStorage.getItem("token");
-  try {
-    await axios({
-      method: "PUT",
-      url: `${API_URL}/api/user/${userId}/security`,
-      withCredentials: true,
-      headers: {
-        ...AUTH_HEADER,
-        "X-JWT-Token": token!,
-      },
-      data,
-    });
   } catch (exception: any) {
     throw new Error(exception.response.data.error);
   }
